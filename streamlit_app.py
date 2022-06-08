47a3dee595a8d1977bb7464cb05bd173ccc96352
Qiusheng Wu <giswqs@gmail.com>
2022-06-08 08:53:44 -0400

    Updated streamlit_call.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,48 +1,40 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
-from apps import home, datasets, split  # import your app modules here
+import leafmap.foliumap as leafmap
 
-st.set_page_config(page_title="Global Surface Water App", layout="wide")
+st.set_page_config(layout="wide")
 
-# A dictionary of apps in the format of {"App title": "App icon"}
-# More icons can be found here: https://icons.getbootstrap.com
+# Customize the sidebar
+markdown = """
+Web App URL: <https://template.streamlitapp.com>
+GitHub Repository: <https://github.com/giswqs/streamlit-multipage-template>
+"""
 
-apps = [
-    {"func": home.app, "title": "Home", "icon": "house"},
-    {"func": datasets.app, "title": "Datasets", "icon": "map"},
-    {"func": split.app, "title": "Split-panel Map", "icon": "layout-split"},
-]
+st.sidebar.title("About")
+st.sidebar.info(markdown)
+logo = "https://streamlit.io/images/brand/streamlit-logo-primary-colormark-darktext.png"
+st.sidebar.image(logo)
 
-titles = [app["title"] for app in apps]
-titles_lower = [title.lower() for title in titles]
-icons = [app["icon"] for app in apps]
+# Customize page title
+st.title("Streamlit for Geospatial Applications")
 
-params = st.experimental_get_query_params()
-
-if "page" in params:
-    default_index = int(titles_lower.index(params["page"][0].lower()))
-else:
-    default_index = 0
-
-with st.sidebar:
-    selected = option_menu(
-        "Main Menu",
-        options=titles,
-        icons=icons,
-        menu_icon="cast",
-        default_index=default_index,
-    )
-
-    st.sidebar.title("About")
-    st.sidebar.info(
-        """
-        **Web App URL:** 
-        <https://gsw.gishub.org>
-        **Source Code available on [GitHub](https://github.com/giswqs/global-surface-water)**
+st.markdown(
     """
-    )
+    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template).
+    """
+)
 
-for app in apps:
-    if app["title"] == selected:
-        app["func"]()
-        break
+st.header("Instructions")
+
+markdown = """
+1. For the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template) or [use it as a template](https://github.com/giswqs/streamlit-multipage-template/generate) for your own project.
+2. Customize the sidebar by changing the sidebar text and logo in each Python files.
+3. Find your favorite emoji from https://emojipedia.org.
+4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
+
+"""
+
+st.markdown(markdown)
+
+m = leafmap.Map(minimap_control=True)
+m.add_basemap("OpenTopoMap")
+m.to_streamlit(height=500)
